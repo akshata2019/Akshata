fdb4d68374cc67008de48337453ebb789a9ba5b6
Shweta Rao <Shwetar@hexaware.com>
2019-04-17 10:15:22 +0530

    Tour Guide App

--- app/src/main/java/com/example/tourguideapp/MainActivity.java
+++ app/src/main/java/com/example/tourguideapp/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.tourguideapp;
 
 import android.app.Activity;
+import android.app.IntentService;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+    EditText editOtp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editOtp=findViewById(R.id.enterOtp);
+        editOtp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
