8ac4ae27f0be0132035f1fcd2ee7ffe5ed4f1d18
Shweta Rao <Shwetar@hexaware.com>
2019-04-18 12:37:20 +0530

    Tour Guide App

--- app/src/main/java/com/example/tourguideapp/MyTourActivity.java
+++ app/src/main/java/com/example/tourguideapp/MyTourActivity.java
@@ -1,12 +1,96 @@
 package com.example.tourguideapp;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public  class MyTourActivity extends Activity {
+import java.util.ArrayList;
+import java.util.List;
+
+public  class MyTourActivity extends AppCompatActivity {
+    DrawerLayout dLayout;
+    TabLayout tabLayout;
+    ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mytour);
+        setNavigationDrawer();
+        tabLayout=findViewById(R.id.tabLayout);
+        viewPager=findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+    private void setNavigationDrawer() {
+        dLayout =  findViewById(R.id.drawer_layout);
+        NavigationView navView =  findViewById(R.id.navigation);
+        dLayout.openDrawer(GravityCompat.START);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                Fragment frag = null;
+                int itemId = menuItem.getItemId();
+                if (itemId == R.id.first) {
+                } else if (itemId == R.id.second) {
+                } else if (itemId == R.id.third) {
+                }
+                Toast.makeText(getApplicationContext(), menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+                if (frag != null) {
+                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                    transaction.replace(R.id.frame, frag);
+                    transaction.commit();
+                    dLayout.closeDrawers();
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new RequestFragment(), "Request");
+        adapter.addFragment(new UpcomingFragment(), "Upcoming");
+        adapter.addFragment(new PastFragment(), "Past");
+        adapter.addFragment(new CancelFragment(), "Cancel");
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
     }
 }
